f403e36367148e5afb1b86987e26ea4d472d97f2
lima9 <max9@openmailbox.org>
2016-11-30 17:49:08 +0100

    Problema curioso con el publicationSchema

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 var mongooseUniqueValidator = require('mongoose-unique-validator');
-var bcrypt = require('bcrypt-nodejs');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
@@ -58,40 +57,6 @@ var userSchema = new Schema({
         ref: 'routineSchema'
     }
 });
-/*
-module.exports.createUser = function (newUser, callback) {
-    bcrypt.genSalt(10, function (err, salt) {
-        if (err) throw err;
-        bcrypt.hash(newUser.password, salt, function (err, hash) {
-            if (err) return err.message;
-            newUser.password = hash;
-            newUser.save(callback);
-        });
-    });
-};
-
-// methods ======================
-// generating a hash
-userSchema.methods.generateHash = function (password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-// this method hashes the password and sets the users password
-userSchema.methods.hashPassword = function (password) {
-    var user = this;
-    // hash the password
-    bcrypt.hash(password, null, null, function (err, hash) {
-        if (err)
-            return next(err);
-        user.local.password = hash;
-    });
-
-};
-
-// checking if password is valid using bcrypt
-userSchema.methods.validPassword = function (password) {
-    return bcrypt.compareSync(password, this.local.password);
-};*/
 
 userSchema.plugin(mongooseUniqueValidator);
 
