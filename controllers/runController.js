d16992f14cb10bef85ebd3c79d7bccbf9bdecf36
arnaucode <idoctnef@openmailbox.org>
2017-01-20 09:40:30 +0100

    quan publiques un run també publiquis una publicació amb imatge pels teus followers

--- controllers/runController.js
+++ controllers/runController.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var runModel = require('../models/runModel');
+var publicationModel = require('../models/publicationModel');
 var userModel = require('../models/userModel');
 //var trainerModel = require('../models/trainerModel');
 var crypto = require('crypto');
@@ -34,7 +35,37 @@ exports.postRun = function (req, res) {
                 /* end of gamification */
                 user.save(function (err, user) {
                     if (err) return res.send(500, err.message);
-                    res.status(200).jsonp(user);
+
+                    //res.status(200).jsonp(user);
+                    //ara farem una publicació ensenyant que ha fet aquest run
+                    var publication = new publicationModel({
+                        title: "new run '" + run.title + "'!",
+                        content: "distance of: " + run.distance + ". View my runs at my profile",
+                        date: new Date(),
+                        user: user._id,
+                        photo: "img/photoRun.png"
+                    });
+                    //fins aquí tenim la variable publication amb els continguts
+                    //ara cal 1r guardar el model publication a la base de dades
+                    publication.save(function (err, publication) {
+                        if (err) return res.send(500, err.message);
+
+                        //i 2n, afegir la id de la publicació generada al user.publications
+                        user.publications.push(publication._id);
+                        /* gamification */
+                        var reward = {
+                            concept: "added new publication to Timeline",
+                            date: Date(),
+                            value: +1
+                        };
+                        user.points.history.push(reward);
+                        user.points.total = user.points.total + 1;
+                        /* end of gamification */
+                        user.save(function (err, user) {
+                            if (err) return res.send(500, err.message);
+                            res.status(200).jsonp(user);
+                        });
+                    });
                 });
             });
         }//end else if
